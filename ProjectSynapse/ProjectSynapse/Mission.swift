f8325e2bcf766dc66a00a41e813d8bac04eb8572
Denis Bystruev <dbystruev@me.com>
2025-08-23 16:04:49 -0700

    Mission: add radius

--- ProjectSynapse/ProjectSynapse/Mission.swift
+++ ProjectSynapse/ProjectSynapse/Mission.swift
@@ -11,12 +11,21 @@ struct Mission: Identifiable {
     let id = UUID()
     let name: String
     let coordinate: CLLocationCoordinate2D
+    let radius: CLLocationDistance
 }
 
 // Hard-coded mission locations in San Francisco for our prototype
 let missionLocations = [
-    Mission(name: "Echo at Coit Tower", coordinate: CLLocationCoordinate2D(latitude: 37.8024, longitude: -122.4058)),
-    Mission(name: "Echo at Ferry Building", coordinate: CLLocationCoordinate2D(latitude: 37.7956, longitude: -122.3937)),
-    Mission(name: "Echo at City Hall", coordinate: CLLocationCoordinate2D(latitude: 37.7792, longitude: -122.4194)),
-    Mission(name: "Echo at Alamo Square", coordinate: CLLocationCoordinate2D(latitude: 37.7761, longitude: -122.4349))
+    Mission(name: "Echo at Coit Tower",
+            coordinate: CLLocationCoordinate2D(latitude: 37.8024, longitude: -122.4058),
+            radius: 100),
+    Mission(name: "Echo at Ferry Building",
+            coordinate: CLLocationCoordinate2D(latitude: 37.7956, longitude: -122.3937),
+            radius: 100),
+    Mission(name: "Echo at City Hall",
+            coordinate: CLLocationCoordinate2D(latitude: 37.7792, longitude: -122.4194),
+            radius: 100),
+    Mission(name: "Echo at Alamo Square",
+            coordinate: CLLocationCoordinate2D(latitude: 37.7761, longitude: -122.4349),
+            radius: 100)
 ]
